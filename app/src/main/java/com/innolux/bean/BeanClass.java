bd9c85e388a4ec75fa3bc1a3af6fb983d844701a
wenge1215 <wenge1215@126.com>
2017-02-20 21:58:54 +0800

    添加模拟数据Bean'Class

---
+++ app/src/main/java/com/innolux/bean/BeanClass.java
@@ -0,0 +1,10 @@
+package com.innolux.bean;
+
+public class BeanClass {
+	public String ID;
+	public String name;
+	public String codeInfo;
+	public String goodNum;		//请购量
+    public String ExistingNum;	//已收
+    public String nowNum;		//实收
+}
